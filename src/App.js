7d7bbfdc537bb0c27ae25ed895f8c6231aff5a02
c3rb1n <ilya_prudnikov97@mail.ru>
2020-11-12 13:34:16 +0300

    add a clock made on React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import Clock from "./Clock/Clock";
+
+const App = props => {
+    return (
+        <div className="app">
+            <Clock/>
+        </div>
+    );
+}
+
+export default App;
